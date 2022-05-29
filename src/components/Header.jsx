dd141ebbb07a5b67100e3fb453d977ed1770bf4b
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2022-05-30 01:19:29 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+  <div className="fixed z-50 w-screen bg-slate-400 p-6 px-16">
+      {/* pc */}
+      <div className="hidden md:flex w-full h-full ">
+
+      </div>
+      {/* mobile */}
+      <div className="flex md:hidden w-full h-full ">
+
+      </div>
+  </div>
+  );
+};
+
+export default Header;
