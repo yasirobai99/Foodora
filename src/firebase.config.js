81dbd70c6f998be859e61b9cd037c8db8bfa436d
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2022-10-21 19:19:04 +0530

    final Commit

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,15 +2,14 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-    apiKey: "AIzaSyCIPSXdcbQmbUF9plHlzgINH9iVXAIL9hw",
-    authDomain: "foodapp-cd043.firebaseapp.com",
-    databaseURL: "https://foodapp-cd043-default-rtdb.firebaseio.com",
-    projectId: "foodapp-cd043",
-    storageBucket: "foodapp-cd043.appspot.com",
-    messagingSenderId: "103111415308",
-    appId: "1:103111415308:web:1c6a9d536ba24c90674029",
-    measurementId: "G-8GPMGJFVE9"
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 
